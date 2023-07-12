5ad9faed25ba10ec5de0b7c2c28dcb97f5ff184a
thomas.opdebeeck <thomas.opdebeeck@adflumine.com>
2023-07-12 14:24:48 +0200

    fix last problem

--- src/utils/hooks/useInitialize.utils.ts
+++ src/utils/hooks/useInitialize.utils.ts
@@ -1,6 +1,6 @@
 import {useRef} from "react";
 
-export const useInitialize = <T>(initialValues: T, initializer: (x: T) => any) => {
+export const useInitialize = <T>(initialValues: T, initializer: (x: T) => void) => {
     const isInitialized = useRef(false)
     if (!isInitialized.current && initialValues) {
         initializer(initialValues)
